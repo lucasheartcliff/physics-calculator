5131fbee05de5229f0aaf7456f6445f2c32acc63
Lucas Morais <lucas.morais@intelie.com.br>
2021-08-31 21:46:44 -0300

    adding methods

--- src/nodes/radius.ts
+++ src/nodes/radius.ts
@@ -2,10 +2,25 @@ import { Node } from '../types/index'
 
 export const radiusMethods: Node[] = [
   {
-    parameters: { mass: true, magneticField: true, charge: true, speed: true },
+    parameters: {
+      mass: true,
+      magneticField: true,
+      charge: true,
+      speed: true,
+    },
     result: { radius: true },
     calculate: ({ mass, magneticField, charge, speed }) => ({
       radius: (speed * mass) / (Math.abs(charge) * magneticField || 1),
+    }),
+  },
+  {
+    parameters: {
+      angle: true,
+      distance: true,
+    },
+    result: { radius: true },
+    calculate: ({ angle, distance }) => ({
+      radius: (distance * angle) / 360,
     }),
   },
 ]
